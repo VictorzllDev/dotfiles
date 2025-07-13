a3957402b92eab46b17de517345ec8598d6c3601
VictorzllDev <VictorzllDev@gmail.com>
2025-07-13 19:15:54 -0300

    refactor(scripts): update bar launch and system scripts
    - Updated Bar.bash to launch new three-bar setup (left/center/right)
    - Simplified ScreenLocker to use static wallpapers directory
    - Removed deprecated bluetooth.sh script (replaced by new polybar
    implementation)
    - Cleaned up script dependencies and paths

--- bspwm/src/scripts/Bar.bash
+++ bspwm/src/scripts/Bar.bash
@@ -1,5 +1,7 @@
 #!/bin/sh
 # This file launch the bar/s
 for mon in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$mon polybar -q emi-bar &
+	MONITOR=$mon polybar -q left &
+	MONITOR=$mon polybar -q center &
+	MONITOR=$mon polybar -q right &
 done
